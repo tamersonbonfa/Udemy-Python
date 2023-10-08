211a4658b45d96e1a4d57ae8ea4162c65e4140ec
Tamerson Bonfa <tamersonboonfa@outlook.com>
2023-10-08 17:15:49 -0300

    first commit

--- Classes/CLASSROOM28_FOR.py
+++ Classes/CLASSROOM28_FOR.py
@@ -1,3 +1,4 @@
+"""
 # senha_salva = '123456'
 # senha_digitada = ''
 # repeticoes = 0
@@ -9,6 +10,8 @@
 
 # print(repeticoes)
 # print('Aquele laço acima pode ter repetições infinitas')
+"""
+
 texto = 'Python'
 
 novo_texto = ''
@@ -16,3 +19,17 @@ for letra in texto:
     novo_texto += f'*{letra}'
     print(letra)
 print(novo_texto + '*')
+
+for i in range(10):
+    if i == 2:
+        print('i é 2, pulando...')
+        continue
+
+    if i == 8:
+        print('i é 8, seu else não executará')
+        break
+
+    for j in range(1, 3):
+        print(i, j)
+else:
+    print('For completo com sucesso!')
